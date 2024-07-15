4a03a77f24b2393718f0cc45cb8fdf0121561abb
YooInKeun <keun0390@naver.com>
2024-07-15 23:57:58 +0900

    chore: change reset button text

--- src/components/ControlButtons.js
+++ src/components/ControlButtons.js
@@ -3,7 +3,7 @@ import React from 'react';
 function ControlButtons({ onInit, onGo, isRunning }) {
   return (
     <div className="control-buttons">
-      <button className='init' onClick={onInit}>초기화</button>
+      <button className='init' onClick={onInit}>RESET!</button>
       <button className={isRunning ? 'stop' : 'go'} onClick={onGo}>{isRunning ? 'STOP!' : 'GO!'}</button>
     </div>
   );
